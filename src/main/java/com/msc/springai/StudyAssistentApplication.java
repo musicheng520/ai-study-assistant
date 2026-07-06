112a12855ef5e5d6e33bfaff20f84a81f259aa95
Sicheng Mu <musicheng520@gmail.com>
2026-07-06 16:49:39 +0800

    init spring boot backend

---
+++ src/main/java/com/msc/springai/StudyAssistentApplication.java
@@ -0,0 +1,13 @@
+package com.msc.springai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyAssistentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyAssistentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msc/springai/StudyAssistentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msc.springai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyAssistentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
